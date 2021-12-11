9dccf58a40ae2a414dda9613fbb9faee37490314
Daniel <daniel@iMac-de-Daniel.local>
2021-12-11 15:00:26 -0300

    feat: complete chalenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface SideBarProps {
+  genres: Genre[];
+  onClickButton: (arn: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  onClickButton,
+}: SideBarProps) {
+  function handleClickButton(genreId: number) {
+    onClickButton(genreId);
+  }
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
